acf7a7750f7d4b4c6601ec4b5a04a38b5275cf1c
jspaz <josephpazjj@gmail.com>
2025-04-03 02:16:24 -0400

    feat: add balance decimal user;

--- src/db/schema/user.schema.ts
+++ src/db/schema/user.schema.ts
@@ -1,11 +1,17 @@
 import { relations } from 'drizzle-orm';
-import { mysqlTable, timestamp, varchar } from 'drizzle-orm/mysql-core';
+import {
+  decimal,
+  mysqlTable,
+  timestamp,
+  varchar,
+} from 'drizzle-orm/mysql-core';
 import { transaction } from './transaction.schema';
 import { reversal } from './reversal.schema';
 
 export const user = mysqlTable('user', {
   id: varchar('id', { length: 36 }).primaryKey().notNull(),
   name: varchar('name', { length: 255 }).notNull(),
+  balance: decimal('balance', { precision: 18, scale: 2 }).notNull(),
   email: varchar('email', { length: 255 }).unique().notNull(),
   password: varchar('password', { length: 255 }).notNull(),
   createdAt: timestamp('createdAt').notNull(),
